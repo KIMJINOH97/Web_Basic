ed8c32b503d30853e90aa252e1734e1b8c0d9704
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-07-06 03:03:37 +0900

    웹기초 문법

--- Java_Script/Ending.js
+++ Java_Script/Ending.js
@@ -1,11 +1,35 @@
-var word = prompt("단어를 입력하세요.")
+var 바디 = document.body;
+var 단어 = document.createElement('div');
+단어.textContent = '제로초';
+document.body.append(단어);
+var 입력창 = document.createElement('input');
+document.body.append(입력창);
+var 버튼 = document.createElement('button');
+버튼.textContent = '입력!';
+document.body.append(버튼);
+var 결과창 = document.createElement('div');
+document.body.append(결과창);
+
+버튼.addEventListener('click', function 콜백함수 (){
+    if (단어.textContent[단어.textContent.length - 1] === 입력창.value[0]) {
+        결과창.textContent = '딩동댕';
+        단어.textContent = 입력창.value;
+    }
+    else{
+        결과창.textContent = '땡';
+    }
+});
+
+/*
+var word = '킹지노'
 while (1) {
-    var connect = prompt(word)
+    var connect = prompt(word);
     if (connect[0] === word[word.length - 1]) {
         alert("딩동댕");
-        word = answer;
+        word = connect;
     }
     else {
         alert("땡");
     }
 }
+*/
--- Java_Script/calculator.js
+++ Java_Script/calculator.js
@@ -1,3 +1,46 @@
+var 숫자1 = Math.ceil(Math.random() * 9) + 1;
+var 숫자2 = Math.ceil(Math.random() * 9) + 1;
+var 결과 = 숫자1 * 숫자2;
+
+var 바디 = document.body;
+var 단어 = document.createElement('div');
+단어.textContent = String(숫자1) + '곱하기' + String(숫자2) + '는?';
+바디.append(단어);
+
+var 폼 = document.createElement("form");
+바디.append(폼);
+
+var 입력창 = document.createElement('input');
+입력창.type = 'Number';
+폼.append(입력창);
+
+var 버튼 = document.createElement('button')
+버튼.textContent = '입력!';
+폼.append(버튼);
+
+var 결과창 = document.createElement('div')
+//결과창.textContent = 결과;
+바디.append(결과창);
+
+폼.addEventListener('submit', function 콜백함수 (e) {
+    e.preventDefault();
+    if(결과 === Number(입력창.value)) {
+        결과창.textContent = '딩동댕';
+        숫자1 = Math.ceil(Math.random() * 9) + 1;
+        숫자2 = Math.ceil(Math.random() * 9) + 1;
+        결과 = 숫자1 * 숫자2;
+        단어.textContent = String(숫자1) + '곱하기' + String(숫자2) + '는?';
+        입력창.value = '';
+        입력창.focus();
+    }
+    else{
+        결과창.textContent = '땡';
+        입력창.value = '';
+        입력창.focus();
+    }
+});
+
+/*
 while (1) {
     var num1 = Math.ceil(Math.random() * 9) + 1
     var num2 = Math.ceil(Math.random() * 9) + 1
@@ -13,3 +56,4 @@ while (1) {
         }
     }
 }
+*/
